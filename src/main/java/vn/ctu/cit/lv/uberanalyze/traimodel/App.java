e21dbf88f5e0ee9614d197238c9a4bf8fa9d619d
PhamDuyKhang <khangy9a1@gmail.com>
2018-06-20 13:28:32 +0700

    int

---
+++ src/main/java/vn/ctu/cit/lv/uberanalyze/traimodel/App.java
@@ -0,0 +1,13 @@
+package vn.ctu.cit.lv.uberanalyze.traimodel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
